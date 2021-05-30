a67331bd8d1caae2f2ec316b478866dfeb8c28a8
Jeffrey Lu <hi@jeffreyclu.com>
2021-05-30 16:53:29 -0400

    add food route

---
+++ server/routes/food.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const foodController = require('../controllers/foodController');
+
+const foodRouter = express.Router();
+foodRouter.get('/all', 
+  foodController.getFoods, 
+  (req, res) => res.status(200).send(res.locals.result)
+)
+
+foodRouter.get('/one/:id',
+  foodController.getFood,
+  (req, res) => res.status(200).send(res.locals.result)
+)
+
+foodRouter.post('/one',
+  foodController.postFood,
+  (req, res) => res.status(200).send(res.locals.result)
+)
+
+foodRouter.put('/one/:id',
+  foodController.putFood,
+  (req, res) => res.status(200).send(res.locals.result)
+)
+
+foodRouter.delete('/one/:id',
+  foodController.deleteFood,
+  (req, res) => res.status(200).send(res.locals.result)
+)
+
+module.exports = foodRouter;
