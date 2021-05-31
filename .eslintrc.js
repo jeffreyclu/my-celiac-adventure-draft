ae67a2d81f6fb163a8dfed1e9b6b50027ae48c3d
Jeffrey Lu <hi@jeffreyclu.com>
2021-05-30 22:05:33 -0400

    add eslintrc and prettierrc

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      "react/prop-types": "off",
+      "react/no-set-state": "off",
+      "react/no-unescaped-entities": "off" 
+    }
+};
