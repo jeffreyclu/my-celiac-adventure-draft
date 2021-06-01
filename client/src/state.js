77b35a9a2ec101ee48514375ca61007c27e2686e
Jeffrey Lu <hi@jeffreyclu.com>
2021-06-01 19:50:33 -0400

    add new form state

--- client/src/state.js
+++ client/src/state.js
@@ -4,9 +4,13 @@ const initialState = {
   showForm: false,
   fetched: false,
   formDisabled: false,
+  formEditable: false,
+  showFormItemOverlay: -1,
   error: '',
   success: '',
   foods: [],
+  food: '',
+  formData: {},
 };
 
 export const { useGlobalState } = createGlobalState(initialState);
