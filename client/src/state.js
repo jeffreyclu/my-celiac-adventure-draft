fd6a49822b9417704ee3185fd603a4ccaecda6fe
Jeffrey Lu <hi@jeffreyclu.com>
2021-06-01 00:08:53 -0400

    add global state fields

--- client/src/state.js
+++ client/src/state.js
@@ -1,4 +1,12 @@
 import { createGlobalState } from 'react-hooks-global-state';
 
-const initialState = {};
+const initialState = {
+  showForm: false,
+  fetched: false,
+  formDisabled: false,
+  error: '',
+  success: '',
+  foods: [],
+};
+
 export const { useGlobalState } = createGlobalState(initialState);
