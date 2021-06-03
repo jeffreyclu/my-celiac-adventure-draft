ed2f3d26db0247be02bdf7ee0e80e36691ed049e
Jeffrey Lu <hi@jeffreyclu.com>
2021-06-02 22:04:57 -0400

    add userSchema

---
+++ server/db/userSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  paid: { type: Boolean, required: false },
+  admin: { type: Boolean, required: false },
+});
+
+userSchema.pre('save', function (next) {
+  const user = this;
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+module.exports = mongoose.model('users', userSchema);
