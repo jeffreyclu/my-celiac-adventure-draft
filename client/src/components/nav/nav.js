afa859519f69c160fdc17eb29e5d11a5ed5d8ee2
Jeffrey Lu <hi@jeffreyclu.com>
2021-06-03 19:50:34 -0400

    update nav bar

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NavLogout from './nav-logout';
+
+import styles from './nav.module.css';
 
 export default function Nav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
+      <Link to="/">Home</Link>
+      <Link to="/profile">Profile</Link>
       <NavLogout />
     </nav>
   );
