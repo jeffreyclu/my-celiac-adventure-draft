36775af42e94d5160772beb39203144dc098725e
Jeffrey Lu <hi@jeffreyclu.com>
2021-06-03 14:29:38 -0400

    add admin auth route

--- client/src/pages/app/App.js
+++ client/src/pages/app/App.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import AddFood from '../add-food/add-food';
 import Game from '../game/game';
+import Login from '../login/login';
+import Register from '../register/register';
+import { useGlobalState } from '../../state';
+import {
+  isAuthorized,
+  isAdminAuthorized,
+} from '../../components/auth/utils/index';
 
 import styles from './app.module.css';
+import AdminAuthorized from '../../components/auth/auth-admin';
+import AdminLogin from '../login/admin-login';
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useGlobalState('loggedIn');
+  const [isAdmin, setIsAdmin] = useGlobalState('isAdmin');
+  const [, setCurrentUser] = useGlobalState('currentUser');
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const getLoginStatus = async () => {
+      const authStatus = await isAuthorized();
+      if (authStatus.success) {
+        setLoggedIn(true);
+        setCurrentUser(authStatus.data);
+      }
+      const adminStatus = await isAdminAuthorized();
+      if (adminStatus.success) {
+        setIsAdmin(true);
+      }
+      setLoaded(true);
+    };
+
+    getLoginStatus();
+  }, [loggedIn, setLoggedIn, setCurrentUser, setIsAdmin]);
+
   return (
     <div className={styles.app}>
-      <Router>
-        <Switch>
-          <Route path="/add-food" component={AddFood} />
-          <Route path="/" component={Game} />
-        </Switch>
-      </Router>
+      {loaded && (
+        <>
+          <Router>
+            <Switch>
+              <AdminAuthorized path="/add-food" component={AddFood} />
+              <Route path="/admin-login" component={AdminLogin} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/" component={Game} />
+            </Switch>
+          </Router>
+        </>
+      )}
     </div>
   );
 }
